62d41d779c8c59eb5cd199a8dd3ffdbf4f84b654
zLuuuck <lucas.toterol.rodrigues@gmail.com>
2026-05-14 20:37:34 -0300

    teste commit

--- lib/Personagens/Heroi.dart
+++ lib/Personagens/Heroi.dart
@@ -2,6 +2,8 @@ import 'personagem.dart';
 
 class Heroi extends Personagem {
   String ranking;
-  Heroi(this.ranking) : super();
+
+  Heroi(String nome, int vida, int idade, int stamina, this.ranking)
+      : super(nome, vida, idade, stamina);
   // tem que ter seu poder
 }
