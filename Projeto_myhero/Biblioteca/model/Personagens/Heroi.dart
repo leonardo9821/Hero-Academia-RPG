ab661069a450537ac6ef7311dc2de856a0a7cc5e
leonardo9821 <leonardo25teixeira@gmail.com>
2026-06-06 15:10:11 -0300

    teste

--- Projeto_myhero/Biblioteca/model/Personagens/Heroi.dart
+++ Projeto_myhero/Biblioteca/model/Personagens/Heroi.dart
@@ -2,6 +2,6 @@ import 'personagem.dart';
 
 class Heroi extends Personagem {
   String ranking;
-  Heroi(this.ranking) : super();
+  Heroi(String nome,int vida,int idade ,int stamina,this.ranking) : super(nome, vida , idade,stamina);
   // tem que ter seu poder
 }
